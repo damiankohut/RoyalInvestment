04839129f6fae3fc964fba663ccb925b1a966317
Damian Kohut <damiankohut@gmail.com>
2022-04-13 00:23:04 -0400

    removed uneeded form

--- indexex.js
+++ indexex.js
@@ -7,14 +7,19 @@ const { application_name } = require("pg/lib/defaults")
 
 const app = express()
 app.use(express.json())
-const PORT = 3000
-app.get("/", (req,res) => {
-    res.send("hello")
-})
+const PORT = 5500
+// app.get("/", (req,res) => {
+//     // res.send("hello")
+//      res.json(req.body)
+// })
 app.get('/user', usersController.getUsers)
 
 app.get("/user/:id", usersController.getOneUser)
-app.post('/user', usersController.makeOnePerson)
+
+app.post('/api/user', (req,res) => {
+    res.json(req.body)
+    // res.send("hello")
+})
 
 
 app.put('/todos/:id', (req, res) => {
@@ -24,5 +29,5 @@ app.put('/todos/:id', (req, res) => {
 
 // Add server listen call here
 app.listen(PORT, () => {
-    console.log("List of todos server")
+    console.log(`List of todos server ${PORT}`)
 })
