cb001278b2abf8fc18d435f5c91eae90a6e1dfce
damian kohut <damiankohut@gmai.com>
2023-02-14 15:26:17 -0500

    change

--- urlConstants.js
+++ urlConstants.js
@@ -2,9 +2,9 @@
 const production = {
   url: "royalinvestment-production.up.railway.app",
 };
-  const development = {
-    url: 'http://localhost:3000'
-  };
+const development = {
+  url: "http://localhost:3000",
+};
 
-
-export const config = process.env.NODE_ENV === 'development' ? development : production;
+export const config =
+  process.env.NODE_ENV === "development" ? development : production;
