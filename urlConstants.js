2f10347877ffe89d798a34d8e1f45906cb282683
damian kohut <damiankohut@gmai.com>
2023-02-14 16:45:59 -0500

    last change

--- urlConstants.js
+++ urlConstants.js
@@ -6,5 +6,4 @@ const development = {
   url: "http://localhost:3000",
 };
 
-export const config =
-  process.env.NODE_ENV === "development" ? development : production;
+export const config = production;
